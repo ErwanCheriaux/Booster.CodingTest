796dc838eac690cbf2d1583c23b9503020282a22
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2021-06-10 11:59:24 +1200

    created a new Console App project (.NET Core)

---
+++ src/Booster.CodingTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Booster.CodingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
