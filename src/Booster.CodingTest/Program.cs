6e104a295dc1590371827919a3adf7e0aea28ff4
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2021-06-18 23:02:49 +1200

    printed all information

--- src/Booster.CodingTest/Program.cs
+++ src/Booster.CodingTest/Program.cs
@@ -15,12 +15,34 @@ namespace Booster.CodingTest
 
             while (true)
             {
+                string outcome = "";
+                
                 Console.SetCursorPosition(0, 0);
                 Console.WriteLine($"{StreamAnalyzer.Sentence}");
                 Console.SetCursorPosition(0, 1);
                 Console.WriteLine($"Number of char: {StreamAnalyzer.CountChar}");
                 Console.SetCursorPosition(0, 2);
                 Console.WriteLine($"Number of word: {StreamAnalyzer.CountWord}");
+
+                outcome = "";
+                Console.SetCursorPosition(0, 3);
+                StreamAnalyzer.SmallestWords.ForEach((e) => outcome += e + " ");
+                Console.WriteLine($"Smallest words: {outcome}");
+
+                outcome = "";
+                Console.SetCursorPosition(0, 4);
+                StreamAnalyzer.LargestWords.ForEach((e) => outcome += e + " ");
+                Console.WriteLine($"Largest words: {outcome}");
+
+                outcome = "";
+                Console.SetCursorPosition(0, 5);
+                StreamAnalyzer.FrequentWords.ForEach((e) => outcome += e + " ");
+                Console.WriteLine($"Frequent words: {outcome}");
+
+                outcome = "";
+                Console.SetCursorPosition(0, 6);
+                StreamAnalyzer.FrequentChars.ForEach((e) => outcome += e + " ");
+                Console.WriteLine($"Frequent chars: {outcome}");
             }
         }
     }
