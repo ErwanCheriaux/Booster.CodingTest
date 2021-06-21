c5553d72e5d63fa2024763c40f885f2a18185e69
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2021-06-21 23:29:45 +1200

    added Console color

--- src/Booster.CodingTest/Program.cs
+++ src/Booster.CodingTest/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Colors.Net;
+using static Colors.Net.StringStaticMethods;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -18,11 +20,11 @@ namespace Booster.CodingTest
             while (true)
             {
                 Console.SetCursorPosition(0, 0);
-                Console.WriteLine($"{StreamAnalyzer.Sentence}");
+                ColoredConsole.WriteLine($"{Cyan(StreamAnalyzer.Sentence)}");
                 Console.SetCursorPosition(0, 1);
-                Console.WriteLine($"Number of char: {StreamAnalyzer.CountChar}");
+                ColoredConsole.WriteLine($"{DarkCyan("Number of char: ")}{StreamAnalyzer.CountChar}");
                 Console.SetCursorPosition(0, 2);
-                Console.WriteLine($"Number of word: {StreamAnalyzer.CountWord}");
+                ColoredConsole.WriteLine($"{DarkCyan("Number of word: ")}{StreamAnalyzer.CountWord}");
 
                 PrintList(StreamAnalyzer.SmallestWords, 0, 3, "Smallest words: ");
                 PrintList(StreamAnalyzer.LargestWords, 0, 4, "Largest words: ");
@@ -34,26 +36,28 @@ namespace Booster.CodingTest
 
         private static void PrintList<T>(List<T> list, int x, int y, string message = "")
         {
+            string outcome = "";
             List<T> l = new(list);
             Console.SetCursorPosition(x, y);
-            l.ForEach((e) => message += e + " ");
-            while (message.Length < Console.WindowWidth) { message += " "; }
-            Console.WriteLine($"{message}");
+            l.ForEach((e) => outcome += e + " ");
+            while (outcome.Length < Console.WindowWidth) { outcome += " "; }
+            ColoredConsole.WriteLine($"{DarkCyan(message)}{outcome}");
         }
 
         private static void PrintCharFrequency(int x, int y, string message = "")
         {
             Console.SetCursorPosition(x, y++);
-            Console.WriteLine($"{message}");
+            while (message.Length < Console.WindowWidth) { message += " "; }
+            ColoredConsole.WriteLine($"{DarkCyan(message)}");
 
             List<char> l = new(StreamAnalyzer.FrequentChars);
 
             foreach (char c in l)
             {
                 Console.SetCursorPosition(x, y++);
-                message = $"'{c}' {StreamAnalyzer.GetCharFrequency(c)}%";
+                message = $"{StreamAnalyzer.GetCharFrequency(c)}%";
                 while (message.Length < Console.WindowWidth) { message += " "; }
-                Console.WriteLine($"{message}");
+                ColoredConsole.WriteLine($"'{Red(c.ToString())}' {message}");
             }
         }
     }
